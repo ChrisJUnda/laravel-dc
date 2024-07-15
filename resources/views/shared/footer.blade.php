75b8174b2ed3e7483c289bcf44a18a881dd6c973
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-15 10:08:58 +0200

    [UPD] Scaffholding oage created

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="py-3 my-4">
+    <div class="container-fluid">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="" class="nav-link px-2 text-body-secondary">Fumetti</a></li>
+            <li class="nav-item"><a href="" class="nav-link px-2 text-body-secondary">Fumetti</a></li>
+            <li class="nav-item"><a href="" class="nav-link px-2 text-body-secondary">Fumetti</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">&copy; 2024 Company, Inc</p>
+    </div>
+</footer>
