f4b2964b26a6a1cbbed6290123c39141fd30fc42
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-15 11:31:50 +0200

    [UPD] Add Comic Seeder

--- database/seeders/ComicSeedder.php
+++ database/seeders/ComicSeedder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,17 @@ class ComicSeedder extends Seeder
     public function run(): void
     {
         $comics = config('comics');
+        foreach ($comics as $comic_data) {
+            $comic = new Comic();
+            $comic->title = $comic_data['title'];
+            $comic->description = $comic_data['description'];
+            $comic->thumb = $comic_data['thumb'];
+            $comic->price = $comic_data['price'];
+            $comic->series = $comic_data['series'];
+            $comic->sale_date = $comic_data['sale_date'];
+            $comic->type = $comic_data['type'];
+            $comic->artists = json_encode($comic_data['artists']);
+            $comic->save();
+        }
     }
 }
